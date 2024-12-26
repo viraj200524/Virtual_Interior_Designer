8fc4247c7909c54f1f96130e4836dc90420d0ca1
viraj200524 <virajv2005@gmail.com>
2024-12-26 21:22:05 +0530

    Landing-Page initial

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LandingPage from './components/Landing-Page/landing'
+
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
